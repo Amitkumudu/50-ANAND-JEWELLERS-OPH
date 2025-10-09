0c492ef4bc33f350c83359e66408d59f9e3f38a3
Amitkumudu <amit@kumudu.in>
2025-10-09 12:49:17 +0530

    first commit

---
+++ 50-Anand Jewellers-bohra/invoice/dsinvoice.vb
@@ -0,0 +1,24 @@
+﻿Partial Class dsinvoice
+    Partial Class rptsessionDataTable
+
+        Private Sub rptsessionDataTable_ColumnChanging(ByVal sender As System.Object, ByVal e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
+            If (e.Column.ColumnName = Me.tagno_alphaColumn.ColumnName) Then
+                'Add user code here
+            End If
+
+        End Sub
+
+    End Class
+
+    Partial Class rptheaderDataTable
+
+        Private Sub rptheaderDataTable_ColumnChanging(ByVal sender As System.Object, ByVal e As System.Data.DataColumnChangeEventArgs) Handles Me.ColumnChanging
+            'If (e.Column.ColumnName = Me.Column1Column.ColumnName) Then
+            '    'Add user code here
+            'End If
+
+        End Sub
+
+    End Class
+
+End Class
